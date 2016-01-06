37974a7ae3307deb1cbabb02921006a0e2775915
mseegan <mseegan@gmail.com>
2016-01-06 11:31:53 -0800

    all tests passed

--- lib/car.rb
+++ lib/car.rb
@@ -1,5 +1,17 @@
 class Car
 
-  # write Car class code here
+class Car
+
+	attr_accessor :speed
+ 	
+	def initialize
+		@speed = 0
+	end
+
+	def accelerate (speed)
+		@speed = speed
+	end
+
+end
 
 end
--- lib/race.rb
+++ lib/race.rb
@@ -2,6 +2,33 @@ require_relative "car"
 
 class Race
 
-  # write Race class code here
+  
+	attr_accessor :cars
+
+	def initialize
+		@cars = [@car1 = Car.new, @car2 = Car.new]
+		@car1.accelerate(rand(100))
+		@car2.accelerate(rand(100))
+	end
+
+	def winner
+		if @car1.speed > @car2.speed
+			return @car1
+		else
+			return @car2
+		end
+	end
+
+	def loser
+		if @car1.speed < @car2.speed
+			return @car1
+		else
+			return @car2
+		end
+	end
+			
+
+
+end
 
 end
